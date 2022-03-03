e703956e9ef7e9ad08648f05e15b20ab9033a097
Alex <alex.beciana@gmail.com>
2022-03-03 18:55:40 -0500

    Started tests and constructing menus

--- book_cli.rb
+++ book_cli.rb
@@ -5,6 +5,31 @@ class BookCli
 
     def self.run
         BookCli.greeting
+
+        puts "If this is your first time here, please type 'create' (without quotes) to create a NEW folder for your reading list.".yellow
+        puts "\n"
+        puts "Otherwise, type 'view' (without quotes) to view your reading list(s)"
+        puts "\n"
+        puts "PLEASE REMEMBER ALL COMMANDS ARE CASE SENSITIVE".red
+        puts "\n"
+        puts "You may type 'exit' anytime to exit the program. For additional help, please type 'help.'"
+        user_welcome_input = gets.chomp
+
+        case user_welcome_input
+        when "help"
+            BookCli.help_menu
+        else
+            puts "Sorry we didn't understand the command you entered. Please try again!".red
+            puts "\n"
+            puts "Would you like to restart the app? (Y/N)"
+            restart = gets.chomp
+            if restart == "Y" || restart == "y"
+                puts "Restarting the app now ..."
+                CLI.run
+            else
+                puts "No problem, come back anytime."
+            end
+        end
     end
 
     def self.greeting
@@ -23,6 +48,10 @@ class BookCli
         puts ",'  '............   '.'   ............'  ',"
         puts " '''''''''''''''''';''';''''''''''''''''''"
         puts "                    '''"
+    end
+
+    def self.help_menu
+        puts "help"
     end
 
 end
--- spec/book_cli_spec.rb
+++ spec/book_cli_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../book_cli.rb'
-
+require 'pry'
 # frozen_string_literal: true
 
 require 'spec_helper'
@@ -15,6 +15,21 @@ RSpec.describe BookCli do
     describe '.greeting' do
         it 'should be defined' do
             expect(described_class).respond_to?(:greeting)
+        end
+
+        context 'user types "help"' do
+
+            it 'terminal should be "help"' do
+                help_input = "help"
+                allow_any_instance_of(Kernel).to receive(:gets).and_return(help_input)
+                expect(described_class.help_menu).to eq("help")
+            end
+        end
+    end
+
+    describe '.help_menu' do
+        it 'should be defined' do
+            expect(described_class).respond_to?(:help_menu)
         end
     end
 
