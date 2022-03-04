6ce50a5626400610f9ca50537b5772a1d2cbe705
Alex <alex.beciana@gmail.com>
2022-03-04 14:32:04 -0500

    :Able to write to JSON reading list file and access/load it

--- book_cli.rb
+++ book_cli.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'colorize'
+require 'json'
 
 class BookCli
 
@@ -18,8 +19,6 @@ class BookCli
         case user_welcome_input
         when "view"
             fetch_reading_lists
-        when "help"
-            help_menu
         when "exit"
             exit
         else
@@ -58,41 +57,72 @@ class BookCli
         reading_list = Dir.entries("reading_lists")
         if reading_list.length < 3
             puts  "You currently have no reading lists. Type 'create' to create one.\n"
-        else
-            "Here's your reading list:"
-            reading_list.each do |list|
-                # binding.pry
+        end
+        if reading_list.length > 2
+            puts "Here's your reading list:"
+            reading_list.each_with_index do |list, index|
+                if list.length > 2 && File.extname(list) == ".json"
+                    # binding.pry
+                    puts "#{index - 1}: #{JSON.load(File.read("./reading_lists/#{list}"))["name"]}"
+                end
             end
+            puts "Type 'create' to create new one."
+            puts "\n"
+            puts "Or type the number of the reading list you want to view."
         end
 
         user_reading_list_options = gets.chomp
 
-        case user_reading_list_options
-        when "create"
+        if user_reading_list_options.to_i.is_a? Integer
+            get_reading_list(user_reading_list_options)
+        elsif user_reading_list_options == "create"
             create_a_reading_list
-        when "exit"
+        elsif user_reading_list_options == "exit"
             exit
         else
             edge_case_restart_app
         end
+
+        # case user_reading_list_options
+        # when "create"
+        #     create_a_reading_list
+        # when "exit"
+        #     exit
+        # else
+        #     edge_case_restart_app
+        # end
     end
 
     def create_a_reading_list
         puts "Give your reading list a name."
-        new_list_entry = gets.chomp
+        new_list_name = gets.chomp
 
-        file_name = new_list_entry.downcase.split(" ").join("_") + ".json"
+        file_name = new_list_name.downcase.split(" ").join("_") + ".json"
         new_list = File.new("./reading_lists/#{file_name}", "w+")
-
-        new_list.
 
         puts "\n"
         puts "Provide a description for your reading list OR press 'Enter' to leave it blank"
+        new_list_desc = gets.chomp
+
+        file_data = {
+            name: new_list_name,
+            description: new_list_desc,
+            books: []
+        }
+        
+        File.open("./reading_lists/#{file_name}", "w") do |file|
+            file.write(JSON.pretty_generate(file_data))
+        end
     end
 
-    # def get_reading_list
+    def get_reading_list(reading_list_index)
+        reading_list = Dir.entries("reading_lists")
+        binding.pry
+        loaded_file = JSON.load(File.read("./reading_lists/#{reading_list[reading_list_index.to_i + 1]}"))
         
-    # end
+        puts "Importing your reading list"
+        puts ""
+    end
 
     def edge_case_restart_app
         puts "Sorry we didn't understand the command you entered. Please try again!".red
